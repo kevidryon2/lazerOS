62922f6c7547e982ac55e0884ba9d48c1bcd33ea
kevidryon2 <kevidryon2@duck.com>
2023-10-21 11:44:06 +0200

    0.611.4

--- commit.sh
+++ commit.sh
@@ -1,3 +1,8 @@
-v=$(echo $(( `date +"%U"` - 42)).`date +"%u%H"`.$(( `date +"%M"` / 10 ))_$1)
+if test -z $1; then
+	v=$(echo $(( `date +"%U"` - 42)).`date +"%u%H"`.$(( `date +"%M"` / 10 )))
+else
+	v=$(echo $(( `date +"%U"` - 42)).`date +"%u%H"`.$(( `date +"%M"` / 10 ))_$1)
+fi
+
 git add *
 git commit -m "$v"
